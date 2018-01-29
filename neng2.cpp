c2e2fd467ef1d9a402162a2c43b8e918daa3c1ed
Stefan Zweig <stefan.zweig@gmail.com>
2018-01-29 22:50:22 +0800

    string changed 头

--- neng2.cpp
+++ neng2.cpp
@@ -12,15 +12,22 @@ int main()
     int n, M;
     string scores;
     std::vector<string> tokens;
+    std::vector<int> int_tokens;
     cin >> M >> n;
     cin.ignore();
     getline(cin, scores);
     istringstream iss(scores);
-    copy(istream_iterator<string>(iss),
-         istream_iterator<string>(),
-         back_inserter(tokens));
-    sort(tokens.begin(), tokens.end(), greater_equal<string>());
+    // copy(istream_iterator<string>(iss),
+    //      istream_iterator<string>(),
+    //      back_inserter(tokens));
 
-    cout << tokens[n-1]<<endl;
+    int number;
+    std::vector<int> myNumbers;
+    while (iss >> number)
+        myNumbers.push_back(number);
+    
+    sort(myNumbers.begin(), myNumbers.end(), greater_equal<int>());
+    // sort(tokens.begin(), tokens.end(), greater_equal<string>());
+    cout << myNumbers[n-1]<<endl;
     return 0;
 }
