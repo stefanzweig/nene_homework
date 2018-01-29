9dc2f822c15afb048038aab9a5dee58d00b1c458
Stefan Zweig <stefan.zweig@gmail.com>
2018-01-29 21:57:53 +0800

    committed to neng

--- neng2.cpp
+++ neng2.cpp
@@ -16,19 +16,11 @@ int main()
     cout << "Type your scores: ";
     cin.ignore();
     getline(cin, scores);
-    cout << scores << endl;
     istringstream iss(scores);
-    // copy(istream_iterator<string>(iss),
-    //      istream_iterator<string>(),
-    //      ostream_iterator<string>(cout, "\n"));
     copy(istream_iterator<string>(iss),
          istream_iterator<string>(),
          back_inserter(tokens));
     sort(tokens.begin(), tokens.end(), greater_equal<string>());
-    for(int i = 0 ; i < tokens.size(); i ++)
-    {
-        cout<<"("<<tokens[i]<<")\n";
-    }
 
     cout << tokens[n-1]<<endl;
 
